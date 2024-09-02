9974788b3df60fc27df8d6aaa5f400fd0d5ab7d1
Oh...Yeah!!! <614988210@qq.com>
2024-09-02 22:05:11 +0800

    +1

---
+++ SupermarketManagerApi - idea/src/main/java/com/rabbiter/market/common/config/BaseWebConfig.java
@@ -0,0 +1,73 @@
+package com.rabbiter.market.common.config;
+
+import com.rabbiter.market.common.sercurity.interceptor.EmpLoginInterceptor;
+import com.rabbiter.market.common.util.PathUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 解决：1.跨域问题
+ */
+@Configuration
+public class BaseWebConfig implements WebMvcConfigurer{
+    @Bean
+    public EmpLoginInterceptor empLoginInterceptor(){
+        return new EmpLoginInterceptor();
+    }
+
+    /**
+     * 拦截器可能没使用的原因：因为拦截器配置好之后，会导致配置全局Cors失效
+     * @param registry
+     */
+
+    //注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(empLoginInterceptor())
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/","/login")
+//                .excludePathPatterns("/files/**")
+//                .excludePathPatterns("/static/**");
+    }
+
+    //跨域访问配置
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            //重写父类提供的跨域请求处理的接口
+            public void addCorsMappings(CorsRegistry registry) {
+                //添加映射路径
+                registry.addMapping("/**")
+                        //放行哪些原始域
+                        .allowedOrigins("*")
+                        //是否发送Cookie信息
+                        .allowCredentials(true)
+                        //放行哪些原始域(请求方式)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
+                        //放行哪些原始域(头部信息)
+                        .allowedHeaders("*")
+                        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                        .exposedHeaders("Header1", "Header2");
+            }
+        };
+    }
+
+    /**
+     * 静态资源映射
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String winPath = PathUtils.getClassLoadRootPath() + "/src/main/resources/static/files/";
+
+        //第一个方法设置访问路径前缀，第二个方法设置资源路径
+        registry.addResourceHandler("/files/**").
+                addResourceLocations("file:" + winPath);
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
---
+++ SupermarketManagerApi - idea/src/main/java/com/rabbiter/market/common/sercurity/interceptor/EmpLoginInterceptor.java
@@ -0,0 +1,107 @@
+package com.rabbiter.market.common.sercurity.interceptor;
+
+
+import com.rabbiter.market.common.constants.HttpStatus;
+import com.rabbiter.market.common.redis.constants.RedisKeys;
+import com.rabbiter.market.common.redis.service.RedisTemplateService;
+import com.rabbiter.market.common.sercurity.annotation.HasPermisson;
+import com.rabbiter.market.common.sercurity.annotation.NoRequireLogin;
+import com.rabbiter.market.common.web.response.JsonResult;
+import com.rabbiter.market.domain.personnel_management.employee.Employee;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EmpLoginInterceptor implements HandlerInterceptor {
+    @Autowired
+    private RedisTemplateService redisTemplateService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if (!(handler instanceof HandlerMethod)) {
+            //optinos请求
+            return true;
+        }
+
+        //允许跨域
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        //请求或响应消息不能缓存(有助于用户获取最新数据)
+        response.setHeader("Cache-Control", "no-cache");
+        response.setCharacterEncoding("UTF-8");
+        //设置返回类型(响应体中的数据是JSON格式的)
+        response.setContentType("application/json");
+
+        //获取token
+        String token = request.getHeader("token");
+
+        /**
+         * 也就是说，根据请求，可以通过handler的反射获取到特定Controller类中某个方法上的注解，
+         * 如果存在该注解，则不进行拦截，如果不存在该注解，则进行拦截
+         */
+        //是否贴有不必登录的注解
+        HandlerMethod handler1 = (HandlerMethod) handler;
+        NoRequireLogin noRequireLogin = handler1.getMethodAnnotation(NoRequireLogin.class);
+
+        if (noRequireLogin != null) {
+            //贴有注解
+            return true;
+
+        } else {
+            if (StringUtils.hasText(token)) {
+                String value2 = redisTemplateService.getCacheObject(token);
+                if (!StringUtils.hasText(value2)) {
+                    JsonResult res = JsonResult.error(HttpStatus.CODE_BUSINESS_ERROR, "请先登录");
+                    String result = JSONObject.toJSONString(res);
+                    response.getWriter().println(result);
+                    response.getWriter().flush();
+                    return false;
+                } else {
+                    //缓存有值,延长存储时间
+                    redisTemplateService.expire(token, RedisKeys.LOGIN_USER.getTimeout(), RedisKeys.LOGIN_USER.getTimeUnit());
+                }
+            } else {
+                //没有token
+                JsonResult res = JsonResult.error(HttpStatus.CODE_BUSINESS_ERROR, "请先登录");
+                String result = JSONObject.toJSONString(res);
+                response.getWriter().println(result);
+                response.getWriter().flush();
+                return false;
+            }
+        }
+        //是否贴有HasPermisson注解
+        HasPermisson hasPermisson = handler1.getMethodAnnotation(HasPermisson.class);
+        if (hasPermisson != null) {
+            if (!StringUtils.hasText(token)) {
+                //没有token
+                JsonResult res = JsonResult.error(HttpStatus.CODE_BUSINESS_ERROR, "请先登录");
+                String result = JSONObject.toJSONString(res);
+                response.getWriter().println(result);
+                response.getWriter().flush();
+                return false;
+            }
+            Employee employee = JSONObject.parseObject(redisTemplateService.getCacheObject(token), Employee.class);
+            //判断是否是系统管理员
+            if (employee.getIsAdmin()) {
+                return true;
+            }
+            String value = hasPermisson.value();
+            boolean contains = employee.getFlags().contains(value);
+            if (!contains) {
+                JsonResult res = JsonResult.error(HttpStatus.CODE_BUSINESS_ERROR, "您没有权限操作");
+                String result = JSONObject.toJSONString(res);
+                response.getWriter().println(result);
+                response.getWriter().flush();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
